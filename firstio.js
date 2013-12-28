c7ed465e548f24c9605b07a30d94dfdb8a774b3b
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2013-12-28 18:01:27 -0300

    Some comments for the excercises.

--- firstio.js
+++ firstio.js
@@ -1,3 +1,10 @@
+// Write a program that uses a single synchronous filesystem operation to
+// read a file and print the number of newlines it contains to the
+// console (stdout), similar to running `cat file | wc -l`.
+
+// The full path to the file to read will be provided as the first
+// command-line argument.
+
 var fs = require('fs');
 var lines = fs.readFileSync(process.argv[2]).toString().split('\n').length - 1;
 console.log(lines);
